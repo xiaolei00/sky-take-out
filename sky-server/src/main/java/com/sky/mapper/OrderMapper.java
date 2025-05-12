385df84a4341edbe68cb003cce63b84ae39852b4
xiaolei00 <1578160665@qq.com>
2025-05-12 16:35:31 +0800

    缓存菜品

---
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -0,0 +1,43 @@
+package com.sky.mapper;
+
+import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.entity.Orders;
+import org.apache.ibatis.annotations.Mapper;
+import com.github.pagehelper.Page;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface OrderMapper {
+
+    /**
+     * 插入一条数据
+     * @param orders
+     */
+    void insert(Orders orders);
+
+    /**
+     * 分页条件查询并按下单时间排序
+     * @param ordersPageQueryDTO
+     */
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
+
+    /**
+     * 根据id查询订单
+     * @param id
+     */
+    @Select("select * from orders where id=#{id}")
+    Orders getById(Long id);
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
+
+    /**
+     * 根据状态统计订单数量
+     * @param status
+     */
+    @Select("select count(id) from orders where status = #{status}")
+    Integer countStatus(Integer status);
+}
